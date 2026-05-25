b2ec24e9dbc184be0ef8b07b9327346f582d5705
Zahid <zainab.12.zahid@gmail.com>
2026-05-26 00:12:07 +0100

    feat: ajout CorsConfig pour tous les microservices

---
+++ restaurant-service/src/main/java/com/example/restaurantservice/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.example.restaurantservice.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowedOrigins(Arrays.asList(
+            "https://chic-pavlova-e40bb6.netlify.app",
+            "http://localhost:3000"
+        ));
+        
+        config.setAllowedMethods(Arrays.asList(
+            "GET", "POST", "PUT", "DELETE", "OPTIONS"
+        ));
+        
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        
+        return new CorsFilter(source);
+    }
+}
